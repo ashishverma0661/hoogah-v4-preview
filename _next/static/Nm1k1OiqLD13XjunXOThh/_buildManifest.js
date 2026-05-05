0534442fe86b13cfe0ed09a26f94aab773ab2549
Ashish Verma <info@thegrowthengine.net>
2026-05-05 18:16:20 +0530

    preview: hover-image fix (source 40d1790)

---
+++ _next/static/Nm1k1OiqLD13XjunXOThh/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/hoogah-v4-preview//_next/:path+",
+        "destination": "/hoogah-v4-preview/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
